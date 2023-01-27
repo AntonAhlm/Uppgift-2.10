2c8615657ccc911311fcf216ba7a867db49df8b4
Anton <>
2023-01-27 08:24:56 +0100

    Add project files.

---
+++ Uppgift 2.10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vilken är din favoritbok? ");
+            string bok = Console.ReadLine();
+            Console.WriteLine(bok);
+
+
+        }
+    }
+}
